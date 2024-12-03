13725b8e1da391a7453321fba19b11b2c4f75d3e
Anna Vasileva <vasilieva.a@icloud.com>
2024-12-03 16:18:01 -0500

    add models

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/news_explorer_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.err);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
